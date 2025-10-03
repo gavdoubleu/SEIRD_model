aaec2243e78e9c93526da869a322c3b5c6b1b14c
gavdoubleu <gavin.a.woolman@durham.ac.uk>
2025-10-03 15:01:50 +0100

    Fitting tests stuff

--- SEIRD_model_files/SEIRD_fitting.py
+++ SEIRD_model_files/SEIRD_fitting.py
@@ -38,7 +38,10 @@ class DeathFitter:
         
 
     def error_function(self) -> float:
-        """Computes the xi^2 error for the death-data fit. 
+        """Computes the xi error for the death-data fit.
+
+        Minimizing xi should be equivalent to maximising the likelihood of seeing the data given a specific model. 
+        
         """
         derivs_for_all_days = self.model_handler.get_derivs_per_day()
         
@@ -60,7 +63,7 @@ class DeathFitter:
                     method='Nelder-Mead',
                     tol=1.0e-12,
                     **kwargs):
-        """Minimizes the xi^2 to obtain the model that best fits the data."""
+        """Minimizes xi to obtain the model that best fits the data."""
         if self.model_handler is None:
             raise Exception("Need to set self.model_handler object in order to fit")
         if self.use_constraints:
@@ -95,24 +98,78 @@ class DeathFitter:
                 if f < self.basin_local_minima[-1][1]:
                     self.basin_local_minima[-1] = (x, f, accept)
 
+    def __write_basin_to_file(self,filename,x,f,accept):
+        with open(filename, 'a') as phile:
+            phile.write("{}, {}, {}\n".format(x, f, accept))
+
+        
+    def __save_basin_hop_iteration_to_file(self, x, f, accept):
+        self.basin_local_minima.sort(key = lambda minima: minima[1])
+        if len(self.basin_local_minima) < self.__num_minima_to_save:
+            if accept:
+                self.basin_local_minima += [(x, f, accept)]
+                self.__write_basin_to_file(self.save_filename, x, f, accept)
+        else:
+            if accept:
+                if f < self.basin_local_minima[-1][1]:
+                    self.basin_local_minima[-1] = (x, f, accept)
+                    self.__write_basin_to_file(self.save_filename, x, f, accept)
+
     def __accept_test(self, f_new, x_new, f_old, x_old):
         if (f_new-f_old < 0) | (np.std(np.array(x_new)-np.array(x_old)) > self.__accept_param_difference_cutoff):
             return True
         else:
             return False
 
+    @classmethod
+    def write_basins_to_file(self, filename, basins):
+        with open(filename, 'a') as phile:
+            for basin in basins:
+                phile.write("{}, {}, {}\n".format(*basin))
+
     def basin_hopping(self,
-                      num_minima_to_save = 1,                      
-                      niter=20,
-                      param_difference_cutoff = 0.001,
-                      minimizer_kwargs = None,
+                      num_minima_to_save: int = 1,                      
+                      niter: int=10,
+                      param_difference_cutoff: float = 0.001,
+                      minimizer_kwargs: any = None,
+                      save_direct_to_file: bool = False,
+                      save_filename: str=None,
                       **kwargs):
+        """Do the basin_hopping algorithm to explore the different possible fits.
+
+        Able to save accepted local minima to a file if desired.
+
+        Args:
+          num_minima_to_save (int, optional):
+            The number of basin_hopping local minima to save in the list of my_fit.basin_local_minima. Default = 1. 
+          niter (int, optional):
+            Number of iterations to do the basin_hopping algorithm over. Default = 10.
+          param_difference_cutoff (float, optional):
+            minimum standard deviation between a new basin local minima and the old one in order to be accepted (stops finding lots of local minima close by to each other). Default = 0.001.
+          minimizer_kwargs (any, optional):
+            kwargs for the minimizer algorithm. Default = None.
+          save_direct_to_file (bool):
+            If True, writes basin local minima directly to a file as soon as they are accepted by __accept_test.
+            This is so that, if the code breaks halfway through, local minima are still stored.
+          save_filename (str, optional):
+            Required only if save_direct_to_file is True. Path to the save file.
+          **kwargs (any):
+            kwargs for the scipy.optimize.basin_hopping method. 
+          
+        
+        """
+        
+        if save_direct_to_file:
+            if save_filename is None:
+                raise ValueError("if save_direct_to_file = True, then the filename to save to (save_filename: str) must not be None")
+            else:
+                self.save_filename = save_filename
         
         # # Changing default kwargs for the minimizer
         if minimizer_kwargs is None:
             minimizer_kwargs = {'method'  : 'cobyqa',
-                                'options' : {'maxiter':10000},
-                                'tol'     : 1.0e-12 }
+                                'options' : {'maxiter':5000},
+                                'tol'     : 1.0e-8 }
         if self.use_constraints:
             if not 'constraints' in minimizer_kwargs.keys():
                 minimizer_kwargs['constraints'] = self.constraints
@@ -124,7 +181,16 @@ class DeathFitter:
             self.basin_local_minima = []
         self.__num_minima_to_save = num_minima_to_save
         self.__accept_param_difference_cutoff = param_difference_cutoff
-        self.basin_opt = basinhopping(self.__fun_to_minimize,
+        if save_direct_to_file:
+            self.basin_opt = basinhopping(self.__fun_to_minimize,
+                                      self.fitting_params,
+                                      niter=niter,
+                                      callback=self.__save_basin_hop_iteration_to_file,
+                                      minimizer_kwargs=minimizer_kwargs,
+                                      accept_test=self.__accept_test,
+                                      **kwargs)
+        else:
+            self.basin_opt = basinhopping(self.__fun_to_minimize,
                                       self.fitting_params,
                                       niter=niter,
                                       callback=self.__save_basin_hop_iteration,
@@ -143,7 +209,11 @@ class DeathFitter:
                  marker_list=None,
                  s=10,
                  **kwargs):
-        """Plot the fit and residuals"""
+        """Plot the fit and residuals.
+
+        Able to plot the fit or the 'rates', where the deaths have been divided by the number of people in the category.
+
+        """
         print("Xi value = {}".format(self.error_function()))
         
         fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12,4))
@@ -242,3 +312,10 @@ class DeathFitter:
 
             ax2.set_title('Residuals'), ax2.set_xlabel('Day'), ax2.set_ylabel('Residuals (deaths/day)')
             ax2.legend()
+
+    
+    def params_to_model(self, params):
+        """Updates self.model to have the parameters given by fitting_params.
+
+        """
+        self.model_handler.set_fitting_params_for_model(params)
